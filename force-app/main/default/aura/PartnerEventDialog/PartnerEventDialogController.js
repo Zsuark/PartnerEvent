d27df98624727b712a6aad867a1f7e5f8d7b3806
Raphael Krausz <krauszraphael@gmail.com>
2018-09-24 03:40:49 +0200

    Added in all needed objects and variables. Save not working yet

--- force-app/main/default/aura/PartnerEventDialog/PartnerEventDialogController.js
+++ force-app/main/default/aura/PartnerEventDialog/PartnerEventDialogController.js
@@ -5,11 +5,14 @@
             null,
             false,
             $A.getCallback(function() {
-                var rec = component.get("v.partnerEventRecord");
+                var rec = component.get("v.partnerEvent");
                 var error = component.get("v.recordError");
                 if (error || (rec === null)) {
                     console.log("Error initializing record template: " + error);
                     return;
+                } else {
+                    console.log("RECORD INITIALISED!!!");
+                    console.log("*** rec: " + JSON.stringify(rec));
                 }
             })
         );
@@ -20,36 +23,70 @@
         // Needed fields
         // Event_Name__c, Capacity__c, Date_Time__c, Description__c
         
-        component.set("v.partnerEventRecord.Event_Name__c", component.find('eventName').get("v.value"));
-        
-        var capacity = parseInt(component.find('capacity').get("v.value"), 10);
-        
-        component.set("v.partnerEventRecord.Capacity__c", capacity);
-        component.set("v.partnerEventRecord.Date_Time__c", component.find('dateTime').get("v.value"));
-        component.set("v.partnerEventRecord.Description__c", component.find('description').get("v.value"));
-        
+		// var eventName   = component.find('eventName').get("v.value");
+        // var capacity    = parseInt(component.find('capacity').get("v.value"), 10);
+        // var dateTime    = component.find("dateTime").get("v.value");
+        // var description = component.get("v.descriptionRichText");
+
+        var partnerEvent = component.get("v.partnerEvent");
+
+        console.log("partnerEvent: " + JSON.stringify(partnerEvent));
+
+
         var tempRec = component.find("forceRecord");
+        /*
+        component.set("v.partnerEvent.Event_Name__c",  eventName);
+        component.set("v.partnerEvent.Capacity__c",    capacity);
+        component.set("v.partnerEvent.Date_Time__c",   dateTime);
+        // component.set("v.partnerEvent.Description__c", description);
+
+        console.log("partnerEvent: " +
+            JSON.stringify(component.get("v.partnerEvent")));
+
+        console.log("forceRecord: " +
+            JSON.stringify(component.find("forceRecord")));
+        */
+
+        /*
+        alert("eventName: "   + eventName);
+        alert("capacity: "    + capacity);
+        alert("dateTime: "    + dateTime);
+        alert("description: " + description);
+        */
+
+        console.log("tempRec: " + JSON.stringify(tempRec));
         tempRec.saveRecord($A.getCallback(function(result) {
             console.log(result.state);
             var resultsToast = $A.get("e.force:showToast");
-            if (result.state === "SUCCESS") {
+            // TODO: REMOVE ME!!!!!!!!! (console.log that is)
+            console.log("*** RESULT: " + JSON.stringify(result));
+            if (result.state === "SUCCESS"  || result.state === "DRAFT") {
                 resultsToast.setParams({
-                    "title": "Saved",
-                    "message": "The record was saved."
+                    "title": "Partner Event Created",
+                    "message": "The Partner Event was created."
                 });
-                resultsToast.fire();                
+                resultsToast.fire();
+                helper.navigateTo(component, result.recordId);
             } else if (result.state === "ERROR") {
                 console.log('Error: ' + JSON.stringify(result.error));
                 resultsToast.setParams({
                     "title": "Error",
                     "message": "There was an error saving the record: " + JSON.stringify(result.error)
                 });
-                resultsToast.fire();
+                resultsToast.fire();         
             } else {
                 console.log('Unknown problem, state: ' + result.state + ', error: ' + JSON.stringify(result.error));
-            }
+            }           
         }));
         
+    },
+    cancelDialog : function(component, helper) {
+        var homeEvt = $A.get("e.force:navigateToObjectHome");
+        homeEvt.setParams({
+            "scope": "PartnerEvent__c"
+        });
+        homeEvt.fire();
     }
+    
     
 })
